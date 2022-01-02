4f72a2c7322379b8ab4d18327995c496ebd4422a
Anders Hafreager <andershaf@gmail.com>
2022-01-02 09:50:51 +0100

    Cleanup

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,5 @@
 import { MainScene } from 'scenes/MainScene';
 
-// export const GameConfig: Phaser.Types.Core.GameConfig = {
-//   title: 'Tank Wars',
-//   url: 'https://andeplane.github.io/tankwars',
-//   version: '1.0',
-//   width: 800,
-//   height: 600,
-//   backgroundColor: 0x3a404d,
-//   type: Phaser.AUTO,
-//   parent: 'game',
-//   physics: {
-//     default: 'arcade',
-//     arcade: {
-//       gravity: { y: 200 }
-//     }
-//   },
-//   scene: [MainScene]
-// };
-
 export const GameConfig: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   title: 'Tank Wars',
